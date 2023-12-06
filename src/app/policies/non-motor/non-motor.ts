483d7fa221e98eeaafca6a5b6e05b331fc7ea7e2
Manjeet Nandal <Manjeet Nandal@DESKTOP-GL7QKAG>
2023-12-06 15:24:56 +0530

    non-motor routes defined

---
+++ src/app/policies/non-motor/non-motor.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-non-motor',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './non-motor.html',
+  styleUrl: './non-motor.css'
+})
+export class NonMotorComponent {
+
+}
--- src/app/policies/policies.routes.ts
+++ src/app/policies/policies.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 
 import { EntrylistComponent } from './policylist';
-import { AddEditPolicyComponent } from './add-edit-policy';
+import { AddEditPolicyComponent } from './motor/add-edit-policy';
+import { NonMotorComponent } from './non-motor/non-motor';
 // import { AddEditComponent } from './';
 
 export const POLICY_ROUTES: Routes = [
     { path: '', component: EntrylistComponent },
-    { path: 'add', component: AddEditPolicyComponent },
-    { path: 'edit/:id', component: AddEditPolicyComponent }
+    { path: 'motor/add', component: AddEditPolicyComponent },
+    { path: 'motor/edit/:id', component: AddEditPolicyComponent },
+ 
+    { path: 'non-motor/add', component: NonMotorComponent },
+    { path: 'non-motor/edit/:id', component: NonMotorComponent }
+
 ];
