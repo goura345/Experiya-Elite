57e21bceb577a0dc93c9f7c1fa821c5e65e32ae7
Manjeet Nandal <Manjeet Nandal@DESKTOP-GL7QKAG>
2023-12-06 18:24:05 +0530

    dashboard now points their routes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 import { User } from '@app/_models';
 import { AccountService } from '@app/_services';
 import { BaseComponent } from "../base/base";
+import { RouterLink } from '@angular/router';
 
 @Component({
    
     templateUrl: 'home.component.html',
     standalone: true,
-    imports: [BaseComponent]
+    imports: [BaseComponent, RouterLink]
 })
 export class HomeComponent {
     user: User | null;
